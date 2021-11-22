65a86ef8a2f9b84b70245a46a7481fa00f73707c
Masoud <masoudtabatabaei20@gmail.com>
2021-11-22 23:00:17 +0330

    change background of key with press on it

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.addEventListener("keydown" , function(e){
     let key = document.querySelector(".key[data-key='"+e.keyCode+"']");
+    key.classList.add("pressed");
        
     let audio = new Audio(key.dataset.src);
-    audio.play(); 
+    audio.play();
+
+    setTimeout(() => {
+        key.classList.remove("pressed");
+    }, 250);
 });
